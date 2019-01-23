2367221d2245ac6614ed31f27c651f4784b01518
Hieu <hieu@Liams-MacBook-Pro.local>
2019-01-23 13:38:01 +1100

    deploy to heroku

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ server.get('/api/hello', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  server.use(express.static(path.join(__dirname, 'client/build')));
+  server.use(server.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
   server.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
@@ -15,7 +15,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 server.start({ 
-  port: process.env.PORT || 4000
+  port: process.env.PORT || 4000,
+  playground: '/playground'
 }, () => {
   console.log('The server is up!')
 })
